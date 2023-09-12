333f20dbfec03437ee0f59e34f49dbb918564164
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2023-09-12 12:13:53 +0530

    While_Loop & Do_While_Loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,25 @@
+//while loop
+
+// let index =0;
+// while(index<=10){
+//     console.log(`Value of index ${index}`)
+//     index+2;
+// }
+
+
+// how to apply while loop in array
+
+let myArray = ["flash", "superman", "saktiman"];
+let index = 0;
+while(index < myArray.length){
+    console.log(`value is ${myArray[index]} `);
+    index++;
+}
+
+
+// *******************Do while loop**********************
+let score =1;
+do{
+console.log(`Score is ${score}`)
+score++;
+}while(score <= 10)
